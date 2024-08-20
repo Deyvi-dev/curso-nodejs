26d2153f1078f0bf03e4e80cf26d1e8db14e9d70
deyvi-dev <deyvisson.margalhaes@gmail.com>
2024-08-20 00:27:32 -0300

    aula 3

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require("express")
 const app = express()
 
 const port = 3000
+app.use(express.json())
 
 app.get("/", (req, res)=>{
-    res.send("olá a todos!")
+    res.json({message: "olá mundo"})
 })
 
+app.post("/submit", (req, res) =>{
+const {name, age} = req.body
+
+    res.json({message: `meu nome é ${name} tenho ${age} anos`})
+})
 app.listen(port, ()=>{
     console.log(`servidor online na porta http://localhost:${port}`)
 })

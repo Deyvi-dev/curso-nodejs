a4bd0b35ed88a6f36885d409856143a328353206
deyvi-dev <deyvisson.margalhaes@gmail.com>
2024-08-20 03:37:31 -0300

    aula 5

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
-const express = require("express")
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const app = express();
+const prisma = new PrismaClient();
+const port = 3000;
 
-const app = express()
+app.use(express.json());
 
-const port = 3000
-app.use(express.json())
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "erro" });
+  }
+});
 
-app.get("/", (req, res)=>{
-    res.json({message: "olá mundo"})
+app.post("/users", async (req, res) =>{
+    const {name} = req.body
+    try{
+    const user = await prisma.user.create({
+        data:{
+            name
+        }
+    })
+    res.status(201).json({message: "usuario criado",  user})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({message: "error ao criard usuario"})
+    }
 })
 
-app.post("/submit", (req, res) =>{
-const {name, age} = req.body
 
-    res.json({message: `meu nome é ${name} tenho ${age} anos`})
-})
-app.listen(port, ()=>{
-    console.log(`servidor online na porta http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`servidor online na porta http://localhost:${port}`);
+});
